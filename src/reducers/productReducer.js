735d551267d8928d61ff8b9fa315e4791484c6b4
shrutanten <shrutanten.work@gmail.com>
2023-09-18 14:14:51 +0530

    Fix reducer

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,29 +1,27 @@
 const initialState = {
-    products: [],
-    product: {}
+	products: [],
+	product: {}
 };
 
-
-
 const productReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "GET_PRODUCTS":
-            return { ...state, products: action.payload };
-        case "GET_PRODUCT":
-            return { ...state, product: action.payload };
-        case "ADD_PRODUCT":
-            return { ...state, products: [...products, action.payload] }; // [p1, p2, p3, p4]
-        case "UPDATE_PRODUCT":
-            return state.products.map((p) =>
-                p.id === action.payload.id ? action.payload : p
-            );
-        case "DELETE_PRODUCT":
-            const products = state.products.filter((p) => p.id !== action.payload.id); // p1, p3, p4
-            return { ...state, products: products };
-        default:
-            return state.products;
-    }
-}
+	switch (action.type) {
+		case "GET_PRODUCTS":
+			return { ...state, products: action.payload };
+		case "GET_PRODUCT":
+			return { ...state, product: action.payload };
+		case "ADD_PRODUCT":
+			return { ...state, products: [...state.products, action.payload] }; // [p1, p2, p3, p4]
+		case "UPDATE_PRODUCT":
+			return state.products.map(p =>
+				p.id === action.payload.id ? action.payload : p
+			);
+		case "DELETE_PRODUCT": {
+			const products = state.products.filter(p => p.id !== action.payload.id); // p1, p3, p4
+			return { ...state, products: products };
+		}
+		default:
+			return state.products;
+	}
+};
 
 export default productReducer;
-
