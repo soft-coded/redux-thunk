05325ad9f58ff46ee0d51f98d0c163657bb84e0c
shrutanten <shrutanten.work@gmail.com>
2023-09-18 14:13:48 +0530

    fix delete function

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,34 +1,37 @@
 import axios from "axios";
 
 class ProductService {
+	baseUrl = "https://fakestoreapi.com";
 
-    baseUrl = "https://fakestoreapi.com";
+	// GET - all products
+	getProducts = async () => {
+		// states - pending, fullfilled, rejected
+		return await axios.get(this.baseUrl + "/products");
+	};
 
-    // GET - all products
-    getProducts = async () => { // states - pending, fullfilled, rejected
-        return await axios.get(this.baseUrl + "/products");
-    }
+	// GET - product by id
+	getProductById = async id => {
+		// states - pending, fullfilled, rejected
+		return await axios.get(this.baseUrl + "/products/" + id);
+	};
 
-    // GET - product by id
-    getProductById = async (id) => { // states - pending, fullfilled, rejected
-        return await axios.get(this.baseUrl + "/products/" + id);
-    }
+	// POST - Add product
+	addProduct = async newProduct => {
+		// states - pending, fullfilled, rejected
+		return await axios.post(this.baseUrl + "/products", newProduct);
+	};
 
-    // POST - Add product
-    addProduct = async (newProduct) => { // states - pending, fullfilled, rejected
-        return await axios.post(this.baseUrl + "/products", newProduct);
-    }
+	// PUT - update product
+	updateProduct = async product => {
+		// states - pending, fullfilled, rejected
+		return await axios.put(this.baseUrl + "/products/" + product.id, product);
+	};
 
-    // PUT - update product
-    updateProduct = async (product) => { // states - pending, fullfilled, rejected
-        return await axios.put(this.baseUrl + "/products/" + product.id, product);
-    }
-
-    // DELETE - Delete product by id
-    deleteProduct = async (id) => { // states - pending, fullfilled, rejected
-        return await axios.delete(this.baseUrl + "/products/" + product.id);
-    }
+	// DELETE - Delete product by id
+	deleteProduct = async id => {
+		// states - pending, fullfilled, rejected
+		return await axios.delete(this.baseUrl + "/products/" + id);
+	};
 }
 
 export default new ProductService();
-
